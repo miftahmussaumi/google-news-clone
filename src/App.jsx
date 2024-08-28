13e34db9ef87d9bc5f75e996a0fc3111acafd614
miftahmussaumi <miftahmussaumi84@gmail.com>
2024-08-28 14:18:18 +0700

    Menubar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
+import Menubar from "./components/Menubar"
 
 function App() {
   // const [count, setCount] = useState(0)
   return (
     <div className="bg-gray-100 min-h-screen">
-      <Navbar/>
+      <Navbar/> 
+      <Menubar/>
     </div>
+    
   )
 }
 
